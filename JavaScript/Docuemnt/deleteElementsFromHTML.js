b1f6c8da04891c97d547b2f0a5efdd76208fc4e4
BartoszSl <github.bartek@gmail.com>
2023-03-31 20:33:29 +0200

    delete elements

---
+++ JavaScript/Docuemnt/deleteElementsFromHTML.js
@@ -0,0 +1,10 @@
+const div = document.querySelector('div')
+const p = document.querySelector('p')
+
+// removeChild
+
+div.removeChild(p)
+
+// remove 
+
+p.remove() // nie musimy odnosić się do rodzica, tylko możemy od razu usnać interesujacy nas element
