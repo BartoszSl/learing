2426ff5a7a0ceba3abfd6c2d479eb0c751a34e4d
BartoszSl <github.bartek@gmail.com>
2023-03-31 19:14:56 +0200

    Tasks for Js

---
+++ JavaScript/Docuemnt/zadania.js
@@ -0,0 +1,15 @@
+// Zadanie 1 - querySelector // querySelectorAll
+
+// const heading = document.querySelector('h1');
+// const paragrafy = document.querySelectorAll('p');
+
+// const divTest = document.querySelector('.test');
+// const idTest = document.querySelector('.test #test');
+
+// Zadanie 2 - ceateElement // appendChild
+
+const divAppend = document.querySelector('div');
+
+const paragraf = document.createElement('p');
+
+divAppend.appendChild(paragraf)
