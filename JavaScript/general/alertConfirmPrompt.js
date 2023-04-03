c3ef68c9f79d6cb57e3ddbd6ed700d93c9bd2129
BartoszSl <github.bartek@gmail.com>
2023-04-03 19:32:53 +0200

    Create alertConfirmPrompt.js

---
+++ JavaScript/general/alertConfirmPrompt.js
@@ -0,0 +1,9 @@
+// alert('czesc')
+
+// if(confirm('chcesz pizze?')){
+//     console.log('Juz zamawiam');
+// }else {
+//     console.log('Nie to nie lamusie');
+// }
+
+const name = prompt('Cześć, jak masz na imie?')
