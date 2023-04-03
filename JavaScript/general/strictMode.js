ed157868cc4f5cd67669d47bee7ca9798a040f0c
BartoszSl <github.bartek@gmail.com>
2023-04-03 19:33:05 +0200

    Create strictMode.js

---
+++ JavaScript/general/strictMode.js
@@ -0,0 +1,9 @@
+// ustawiamy, aby sprawdzało błędy w dodawaniu let / const w np. petli
+
+'use strict'
+
+const colors = ['red', 'green', 'blue']
+
+for(let color of colors) {
+    console.log(color);
+}
