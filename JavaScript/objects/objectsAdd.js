0bc52f552aa339c0d7cf6cfc9e9086d45f6e805d
BartoszSl <github.bartek@gmail.com>
2023-04-07 15:36:10 +0200

    Class section add elements to object

---
+++ JavaScript/objects/objectsAdd.js
@@ -0,0 +1,26 @@
+// const user = {
+// 	name: 'Klauda',
+// 	age: 23,
+// 	'second-color': 'czerwony',
+// };
+
+// user.country = 'Polska';
+// user['fav-color'] = 'niebieski';
+
+// console.log(user);
+
+
+const dogName = 'Drops'
+
+const dogAge = 5
+
+const dog = {
+    // klucz: wartosc
+    name: dogName,
+    age: dogAge,
+
+    dogName,
+    dogAge
+}
+
+console.log(dog);
