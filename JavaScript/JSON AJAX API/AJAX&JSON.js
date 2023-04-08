b818fcd4dd8adb861fbfbe10fa798d19d8c6af65
BartoszSl <github.bartek@gmail.com>
2023-04-08 13:52:33 +0200

    Ajax & JSON

---
+++ JavaScript/JSON AJAX API/AJAX&JSON.js
@@ -0,0 +1,24 @@
+// AJAX:
+
+// 1. Asynchroniczność JavaScriptu i XML
+// 2. Wysyłanie i pobieranie danych bez konieczności przeładowania strony
+
+const user = {
+    name: 'Elwis',
+    age: 23,
+    car: 'Audi'
+}
+
+// JSON:
+
+// 1. Podobny do obiektu 
+// 2. reprezentuje dane i tylko dane, nie może przyjąć, np. funkcji 
+// 3. Musi być tylko w " ",
+// 4. ' ', ostatnia właściwość bez przecinka
+// 5. rozszerzenie .json
+
+// {
+//     "name": "Elwis",
+//     "age": 23,
+//     "car": "car"
+// }
