37ccd7faa5d74c46ac7fe7588a1419b94a2c3fca
BartoszSl <github.bartek@gmail.com>
2023-04-12 22:10:37 +0200

    Get Post Put in js

---
+++ JavaScript/JSON AJAX API/GETPOSTPUT.js
@@ -0,0 +1,16 @@
+// get - pobiera dane
+// post - wysyła dane
+// put, patch - edycja dancyh
+// delete - usuwa da ne
+
+
+const test = {
+    method: 'POST'
+}
+
+// fetch(url, {
+//     method: 'GET'
+// });
+
+
+fetch(url, test)
