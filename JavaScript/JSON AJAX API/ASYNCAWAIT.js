422cd90298f30a4aa0a5812f96d5e5e39bc5e53f
BartoszSl <github.bartek@gmail.com>
2023-04-13 21:02:40 +0200

    ASYNC AND AWAIT

--- JavaScript/JSON AJAX API/ASYNCAWAIT.js
+++ JavaScript/JSON AJAX API/ASYNCAWAIT.js
@@ -17,13 +17,29 @@ const doubleCheck = () => {
 	});
 };
 
-checkAge(50)
-	.then(() => {
-		console.log('Chyba możesz wejść');
-        return doubleCheck()
-	})
-    .then(res => console.log(res))
-    .then(() => {
-        console.log('Weryfikacja zakończona');
-    })
-	.catch((error) => console.error(error));
+// checkAge(50)
+// 	.then(() => {
+// 		console.log('Chyba możesz wejść');
+//         return doubleCheck()
+// 	})
+//     .then(res => console.log(res))
+//     .then(() => {
+//         console.log('Weryfikacja zakończona');
+//     })
+// 	.catch((error) => console.error(error));
+
+// ASYNC
+
+async function test() {
+	try {
+		await checkAge(12);
+		console.log('Chyma możesz wejśc');
+		await doubleCheck();
+		console.log('Faktycznie, wiek się zgadza.');
+		console.log('Weryfikacja zakończona!');
+	} catch {
+        console.error('Błąd, masz za mało lat')
+    }
+}
+
+test();
