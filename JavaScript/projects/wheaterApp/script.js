6fc2aad7d7a9ca367016e7a6a87f3f7bb1d149c7
BartoszSl <github.bartek@gmail.com>
2023-04-16 08:23:27 +0200

    Added API

--- JavaScript/projects/wheaterApp/script.js
+++ JavaScript/projects/wheaterApp/script.js
@@ -6,3 +6,8 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = '&appid=81deced5c860ace44dd7cdb50bfdf6f0'
+const API_UNITS = '&units=metric'
+
