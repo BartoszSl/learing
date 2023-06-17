895243c39780449869757107146a2e62b11c1f25
BartoszSl <github.bartek@gmail.com>
2023-06-17 21:41:12 +0200

    TypeScript

--- TypeScript/BasicTypes/app.ts
+++ TypeScript/BasicTypes/app.ts
@@ -1,9 +1,14 @@
-function add(n1: number, n2: number) {
-	return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, text: string) {
+	const result = n1 + n2;
+	if (showResult) {
+		return tekst + result;
+	}
 }
 
 const number1 = 6;
 const number2 = 2.5;
+const printResult = true;
+const tekst = 'Wynik to: ';
 
-const result = add(number1, number2)
+const result = add(number1, number2, printResult, tekst);
 console.log(result);
