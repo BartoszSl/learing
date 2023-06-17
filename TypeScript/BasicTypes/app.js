dd4ef8d7d897706dcd93f4374370d2292c7effbb
BartoszSl <github.bartek@gmail.com>
2023-06-17 21:41:07 +0200

    Converted JavaScript

--- TypeScript/BasicTypes/app.js
+++ TypeScript/BasicTypes/app.js
@@ -1,7 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, text) {
+    var result = n1 + n2;
+    if (showResult) {
+        return tekst + result;
+    }
 }
 var number1 = 6;
 var number2 = 2.5;
-var result = add(number1, number2);
+var printResult = true;
+var tekst = 'Wynik to: ';
+var result = add(number1, number2, printResult, tekst);
 console.log(result);
