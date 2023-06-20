2756ab9518394e5ecae42361375707592337f384
BartoszSl <github.bartek@gmail.com>
2023-06-20 17:17:34 +0200

    Converted Object TypeScript

--- TypeScript/BasicTypes/object.js
+++ TypeScript/BasicTypes/object.js
@@ -1,5 +1,23 @@
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 0] = "ADMIN";
+    Roles[Roles["AUTHOR"] = 1] = "AUTHOR";
+    Roles[Roles["RANK1"] = 2] = "RANK1";
+    Roles[Roles["RANK0"] = 3] = "RANK0";
+})(Roles || (Roles = {}));
 var person = {
-    role: [1, 'test']
+    name: 'Bartek',
+    role: Math.floor(Math.random() * 4)
 };
-person.role.push('test2');
-console.log(person.role);
+if (person.role === Roles.ADMIN) {
+    console.log('Jesteś Adminem');
+}
+else if (person.role === Roles.AUTHOR) {
+    console.log('Jesteś Autorem');
+}
+else if (person.role === Roles.RANK1) {
+    console.log('Jesteś Rank1');
+}
+else if (person.role === Roles.RANK0) {
+    console.log('Jesteś Rank0, noobie');
+}
